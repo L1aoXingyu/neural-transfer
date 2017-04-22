b0d0a7c9cd8fd2a14b049678760f9849a60c5dec
Sherlock <sherlockliao01@gmail.com>
2017-04-22 19:51:26 +0800

    update

---
+++ load_img.py
@@ -0,0 +1,22 @@
+import PIL.Image as Image
+import torchvision.transforms as transforms
+
+img_size = 512
+img_loader = transforms.Compose([
+    transforms.Scale(img_size),
+    transforms.ToTensor()
+])
+
+
+def load_img(img_path):
+    img = Image.open(img_path)
+    img = img.resize((img_size, img_size))
+    img = img_loader(img)
+    img = img.unsqueeze(0)
+    return img
+
+
+def show_img(img):
+    img = img.squeeze(0)
+    img = transforms.ToPILImage()(img)
+    img.show()
